60945005080ac5b1f737a36b300a6c49c7e5a6ef
valeriatisch <valeria.tisch97@gmail.com>
2023-12-18 15:39:49 +0100

    feature: reply to comments

--- bugxplainer/ghclient.py
+++ bugxplainer/ghclient.py
@@ -1,5 +1,4 @@
 from typing import Optional
-from dateutil.parser import parse
 
 import requests
 import logging
@@ -19,15 +18,16 @@ class GitHubClient:
             issue_number = 0
             if issues:
                 issue_number = issues[0]["number"]
-                self.issues[issue_number] = {
-                    "body": None,
-                    "comments": None,
-                    "gpt_response": None,
-                }
             return issue_number
         except requests.RequestException as e:
             logging.error(f"Error fetching issue: {e}")
             return 0
+
+    def add_issue_to_cache(self, issue_number: int) -> None:
+        self.issues[issue_number] = {
+            "body": None,
+            "comments": None,
+        }
 
     def post_comment(self, issue_number: int, comment: str) -> Optional[int]:
         url = f"{self.api_url}/{issue_number}/comments"
@@ -58,8 +58,9 @@ class GitHubClient:
             logging.error(f"Error fetching issue content: {e}")
             return ""
 
-    def set_gpt_response(self, issue_number: int, gpt_response: str) -> None:
-        self.issues[issue_number]["gpt_response"] = gpt_response
+    def update_comments(self, issue_number: int, comments: list = None) -> None:
+        comments = comments or self.get_issue_comments(issue_number)
+        self.issues[issue_number]["comments"] = comments
 
     def get_issue_comments(self, issue_number: int) -> list:
         comments_url = f"{self.api_url}/{issue_number}/comments"
@@ -71,20 +72,3 @@ class GitHubClient:
                 f"Failed to fetch comments for issue {issue_number}: {response.status_code}, {response.text}"
             )
             return []
-
-    @staticmethod
-    def find_user_response(comments: list) -> list:
-        chatgpt_last_comment_time = None
-
-        for comment in reversed(comments):
-            if comment["body"].startswith("This is an automated comment by ChatGPT"):
-                chatgpt_last_comment_time = parse(comment["created_at"])
-                break
-
-        responses = []
-        for comment in comments:
-            comment_time = parse(comment["created_at"])
-            if comment_time > chatgpt_last_comment_time:
-                responses.append(comment)
-
-        return responses
